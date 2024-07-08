d4fb458a23c8f1004eeeac4f4bfdf9922b4236fd
jeandrad1 <jesusandradeperez@gmail.com>
2024-07-08 15:23:37 +0200

    mods to free_pipex

--- free.c
+++ free.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:12:24 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/07/08 15:15:39 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:23:22 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,26 @@ void	ft_free_pipex(t_args pipex)
 	int	i;
 
 	i = 0;
-	while (pipex->cmd[i])
+	if (pipex.cmd)
 	{
-		free(pipex->cmd[i]);
-		i++;
+		while (pipex.cmd[i])
+		{
+			free(pipex.cmd[i]);
+			i++;
+		}
+		free(pipex.cmd);
 	}
-	free(pipex->cmd);
-	free(pipex->infile);
-	free(pipex->outfile);
-	free(pipex);
+	if (pipex.infile)
+		free(pipex.infile);
+	if (pipex.outfile)
+		free(pipex.outfile);
+	if (pipex.infile_fd != -1)
+		close(pipex.infile_fd);
+	if (pipex.outfile_fd != -1)
+		close(pipex.outfile_fd);
+	if (pipex.pipe_fd[0] != -1)
+		close(pipex.pipe_fd[0]);
+	if (pipex.pipe_fd[1] != -1)
+		close(pipex.pipe_fd[1]);
+	free(&pipex);
 }

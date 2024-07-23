d6bdf643d0a721564203a83641cdf09c8c802879
jeandrad1 <jesusandradeperez@gmail.com>
2024-07-23 21:27:33 +0200

    small modifications to handle memory and errors. Still works

--- testing.c
+++ testing.c
@@ -12,9 +12,36 @@ typedef struct {
     char *outfile;
 } t_args;
 
+void ft_free_pipex(t_args *pipex) {
+    if (pipex->cmd1) {
+        for (int i = 0; pipex->cmd1[i] != NULL; i++) {
+            free(pipex->cmd1[i]);
+        }
+        free(pipex->cmd1);
+    }
+
+    if (pipex->cmd2) {
+        for (int i = 0; pipex->cmd2[i] != NULL; i++) {
+            free(pipex->cmd2[i]);
+        }
+        free(pipex->cmd2);
+    }
+}
+
+void ft_exit(t_args *pipex, const char *error_message, int exit_code) {
+    ft_free_pipex(pipex);
+    if (error_message) {
+        fprintf(stderr, "%s\n", error_message);
+    }
+    exit(exit_code);
+}
+
 void ft_init_pipex(t_args *pipex) {
     // Initialize pipex structure
-    memset(pipex, 0, sizeof(t_args));
+    pipex->cmd1 = NULL;
+    pipex->cmd2 = NULL;
+    pipex->infile = NULL;
+    pipex->outfile = NULL;
 }
 
 char **split_command(const char *command) {
@@ -45,8 +72,7 @@ void check_and_set_env() {
 
 void ft_parse(int argc, char **argv, t_args *pipex) {
     if (argc != 5) {
-        fprintf(stderr, "Usage: %s infile command1 command2 outfile\n", argv[0]);
-        exit(EXIT_FAILURE);
+        ft_exit(pipex, "Usage: ./pipex infile command1 command2 outfile", EXIT_FAILURE);
     }
     pipex->infile = argv[1];
     pipex->cmd1 = split_command(argv[2]);
@@ -56,8 +82,7 @@ void ft_parse(int argc, char **argv, t_args *pipex) {
     // Check if infile is valid
     int infile_fd = open(pipex->infile, O_RDONLY);
     if (infile_fd == -1) {
-        perror("open infile");
-        exit(EXIT_FAILURE);
+        ft_exit(pipex, "Error: Unable to open infile", EXIT_FAILURE);
     }
     close(infile_fd);
 
@@ -68,20 +93,17 @@ void ft_parse(int argc, char **argv, t_args *pipex) {
 void ft_exec_cmd(t_args *pipex) {
     int pipe_fd[2];
     if (pipe(pipe_fd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
+        ft_exit(pipex, "Error: Unable to create pipe", EXIT_FAILURE);
     }
 
     pid_t pid1 = fork();
     if (pid1 == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
+        ft_exit(pipex, "Error: Unable to fork process for cmd1", EXIT_FAILURE);
     } else if (pid1 == 0) {
         // Child process for command1
         int infile_fd = open(pipex->infile, O_RDONLY);
         if (infile_fd == -1) {
-            perror("open infile");
-            exit(EXIT_FAILURE);
+            ft_exit(pipex, "Error: Unable to open infile in child process", EXIT_FAILURE);
         }
         dup2(infile_fd, STDIN_FILENO);
         dup2(pipe_fd[1], STDOUT_FILENO);
@@ -89,20 +111,17 @@ void ft_exec_cmd(t_args *pipex) {
         close(pipe_fd[0]);
         close(pipe_fd[1]);
         execvp(pipex->cmd1[0], pipex->cmd1);
-        perror("execvp cmd1");
-        exit(EXIT_FAILURE);
+        ft_exit(pipex, "Error: execvp failed for cmd1", EXIT_FAILURE);
     }
 
     pid_t pid2 = fork();
     if (pid2 == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
+        ft_exit(pipex, "Error: Unable to fork process for cmd2", EXIT_FAILURE);
     } else if (pid2 == 0) {
         // Child process for command2
         int outfile_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
         if (outfile_fd == -1) {
-            perror("open outfile");
-            exit(EXIT_FAILURE);
+            ft_exit(pipex, "Error: Unable to open outfile in child process", EXIT_FAILURE);
         }
         dup2(pipe_fd[0], STDIN_FILENO);
         dup2(outfile_fd, STDOUT_FILENO);
@@ -110,27 +129,28 @@ void ft_exec_cmd(t_args *pipex) {
         close(pipe_fd[0]);
         close(pipe_fd[1]);
         execvp(pipex->cmd2[0], pipex->cmd2);
-        perror("execvp cmd2");
-        exit(EXIT_FAILURE);
+        ft_exit(pipex, "Error: execvp failed for cmd2", EXIT_FAILURE);
     }
 
     // Parent process
     close(pipe_fd[0]);
     close(pipe_fd[1]);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-}
 
-void ft_free_pipex(t_args *pipex) {
-    for (int i = 0; pipex->cmd1[i] != NULL; i++) {
-        free(pipex->cmd1[i]);
+    int status1, status2;
+    if (waitpid(pid1, &status1, 0) == -1) {
+        ft_exit(pipex, "Error: waitpid failed for cmd1", EXIT_FAILURE);
     }
-    free(pipex->cmd1);
+    if (waitpid(pid2, &status2, 0) == -1) {
+        ft_exit(pipex, "Error: waitpid failed for cmd2", EXIT_FAILURE);
+    }
 
-    for (int i = 0; pipex->cmd2[i] != NULL; i++) {
-        free(pipex->cmd2[i]);
+    if (status1 != 0) {
+        ft_exit(pipex, "Error: cmd1 failed", status1);
     }
-    free(pipex->cmd2);
+
+    if (status2 != 0) {
+        ft_exit(pipex, "Error: cmd2 failed", status2);
+    }
 }
 
 // Main function
@@ -147,7 +167,6 @@ int main(int argc, char **argv) {
     printf("exec OK\n");
     ft_free_pipex(&pipex);
     printf("free OK\n");
-
     // Return the program
     return (EXIT_SUCCESS);
 }

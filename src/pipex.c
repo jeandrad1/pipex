2a4092813642ceb3d6ed879d00059cae1cf830fe
jeandrad1 <jesusandradeperez@gmail.com>
2024-07-24 15:56:40 +0200

    deleted the messages in the main

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 12:57:09 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/07/24 14:41:04 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/07/24 15:55:59 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,9 @@ int	main(int argc, char **argv, char **env)
 	t_args	pipex;
 
 	pipex.env = env;
-	printf("memset OK\n");
 	ft_init_pipex(&pipex);
-	printf("init OK\n");
 	ft_parse(argc, argv, &pipex);
-	printf("parse OK\n");
 	ft_exec_cmd(&pipex);
-	printf("exec OK\n");
 	ft_free_pipex(&pipex);
-	printf("free OK\n");
-	printf("EXIT_SUCCESS\n");
 	return (EXIT_SUCCESS);
 }

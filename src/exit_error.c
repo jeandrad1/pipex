c3b83172dcbf9988fd10c8132e8c2f9314a8d944
jeandrad1 <jesusandradeperez@gmail.com>
2024-07-26 11:11:49 +0200

    Fixed the exit codes

--- src/exit_error.c
+++ src/exit_error.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 13:12:58 by jeandrad          #+#    #+#             */
-/*   Updated: 2024/07/24 18:49:48 by jeandrad         ###   ########.fr       */
+/*   Updated: 2024/07/26 11:11:08 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	ft_exit(t_args *pipex, const char *error_message, int exit_code)
 	ft_free_pipex(pipex);
 	if (error_message)
 		perror(error_message);
-	exit(WEXITSTATUS(exit_code));
+	exit(exit_code);
 }

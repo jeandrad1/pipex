bd028fe8aa6e578f1f49728458b3e95a063eb91d
jeandrad1 <jesusandradeperez@gmail.com>
2024-07-26 13:08:08 +0200

    Added .sh for testing and improved the testing.c

--- testing.c
+++ testing.c
@@ -1,275 +1,292 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <string.h>
+#include "includes/pipex.h"
 
-typedef struct {
-    char **cmd1;
-    char **cmd2;
-    char *infile;
-    char *outfile;
-} t_args;
+void	ft_free_pipex(t_args *pipex)
+{
+	int	i;
 
-size_t ft_strspn(const char *s, const char *accept) {
-    const char *p, *a;
-    size_t count = 0;
-
-    for (p = s; *p != '\0'; ++p) {
-        for (a = accept; *a != '\0'; ++a) {
-            if (*p == *a) {
-                break;
-            }
-        }
-        if (*a == '\0') {
-            return count;
-        }
-        ++count;
-    }
-    return count;
+	i = 0;
+	if (pipex->cmd1)
+	{
+		while (pipex->cmd1[i] != NULL)
+		{
+			free(pipex->cmd1[i]);
+			i++;
+		}
+		free(pipex->cmd1);
+	}
+	i = 0;
+	if (pipex->cmd2)
+	{
+		while (pipex->cmd2[i] != NULL)
+		{
+			free(pipex->cmd2[i]);
+			i++;
+		}
+		free(pipex->cmd2);
+	}
 }
 
-// Custom implementation of strcspn
-size_t ft_strcspn(const char *s, const char *reject) {
-    const char *p, *r;
-    size_t count = 0;
-
-    for (p = s; *p != '\0'; ++p) {
-        for (r = reject; *r != '\0'; ++r) {
-            if (*p == *r) {
-                return count;
-            }
-        }
-        ++count;
-    }
-    return count;
+void	ft_exit(t_args *pipex, const char *error_message, int exit_code)
+{
+	ft_free_pipex(pipex);
+	if (error_message)
+		perror(error_message);
+	exit(exit_code);
 }
 
-// Custom implementation of memcpy
-void *ft_memcpy(void *dest, const void *src, size_t n) {
-    unsigned char *d = dest;
-    const unsigned char *s = src;
 
-    while (n--) {
-        *d++ = *s++;
-    }
-    return dest;
-}
-char *ft_strtok(char *str, const char *delim) {
-    static char *last;
-    if (str == NULL) {
-        str = last;
-    }
-    if (str == NULL) {
-        return NULL;
-    }
 
-    // Skip leading delimiters
-    str += ft_strspn(str, delim);
-    if (*str == '\0') {
-        return NULL;
-    }
-
-    // Find the end of the token
-    char *end = str + ft_strcspn(str, delim);
-    if (*end == '\0') {
-        last = NULL;
-    } else {
-        *end = '\0';
-        last = end + 1;
-    }
-
-    return str;
+// Initialize the pipex structure
+void	ft_init_pipex(t_args *pipex)
+{
+	pipex->infile = NULL;
+	pipex->outfile = NULL;
+	pipex->cmd1 = NULL;
+	pipex->cmd2 = NULL;
 }
 
-// Custom implementation of strdup
-char *ft_strdup(const char *s) {
-    size_t len = strlen(s) + 1;
-    char *dup = malloc(len);
-    if (dup == NULL) {
-        return NULL;
-    }
-    ft_memcpy(dup, s, len);
-    return dup;
+// Check if the string is empty or contains only spaces
+static int	check_void(const char *str)
+{
+	if (str == NULL)
+		return (1);
+	while (*str)
+	{
+		if (!ft_isspace((unsigned char)*str))
+			return (0);
+		str++;
+	}
+	return (1);
 }
 
-// Custom implementation of realloc
-void *ft_realloc(void *ptr, size_t size) {
-    if (ptr == NULL) {
-        return malloc(size);
-    }
-    if (size == 0) {
-        free(ptr);
-        return NULL;
-    }
+// Check if PATH is set and set it if not it displays an error message and exits
+static void	check_and_set_env(char **env)
+{
+	int	path_found;
 
-    void *new_ptr = malloc(size);
-    if (new_ptr == NULL) {
-        return NULL;
-    }
-
-    // Copy old data to new memory
-    ft_memcpy(new_ptr, ptr, size);
-    free(ptr);
-    return new_ptr;
+	path_found = 0;
+	while (*env)
+	{
+		if (ft_strncmp(*env, "PATH=", 5) == 0)
+		{
+			path_found = 1;
+			break ;
+		}
+		env++;
+	}
+	if (!path_found)
+	{
+		perror("Error: PATH not set");
+		exit(EXIT_FAILURE);
+	}
 }
 
-void ft_free_pipex(t_args *pipex) {
-    if (pipex->cmd1) {
-        for (int i = 0; pipex->cmd1[i] != NULL; i++) {
-            free(pipex->cmd1[i]);
-        }
-        free(pipex->cmd1);
-    }
+// Split the command into an array of strings
+// It uses tokenize_command to split the command by spaces
+// And reallocates the array of strings to store each token
+// It returns the array of strings
+static char	**split_command(const char *command)
+{
+	int		count;
+	char	**args;
 
-    if (pipex->cmd2) {
-        for (int i = 0; pipex->cmd2[i] != NULL; i++) {
-            free(pipex->cmd2[i]);
-        }
-        free(pipex->cmd2);
-    }
+	args = tokenize_command(command, &count);
+	args = allocate_args(args, count);
+	args[count] = NULL;
+	return (args);
 }
 
-void ft_exit(t_args *pipex, const char *error_message, int exit_code) {
-    ft_free_pipex(pipex);
-    if (error_message) {
-        fprintf(stderr, "%s\n", error_message);
-    }
-    exit(exit_code);
+// Parse the arguments
+// Uses the split_command function to split the commands
+// And checks if the infile and outfile can be opened
+// Check the environment variables
+void	ft_parse(int argc, char **argv, t_args *pipex)
+{
+	int	infile_fd;
+	int	i;
+
+	i = 0;
+	if (argc != 5)
+		ft_exit(pipex, "Error: invalid number of arguments", EXIT_FAILURE);
+	if (check_void(argv[2]) || check_void(argv[3]))
+		ft_exit(pipex, "Error: command arguments cannot be empty or whitespace",
+			EXIT_FAILURE);
+	pipex->infile = argv[1];
+	pipex->cmd1 = split_command(argv[2]);
+	pipex->cmd2 = split_command(argv[3]);
+	pipex->outfile = argv[4];
+	infile_fd = open(pipex->infile, O_RDONLY);
+	if (infile_fd == -1)
+		ft_exit(pipex, "Error: Unable to open infile", EXIT_FAILURE);
+	close(infile_fd);
+	check_and_set_env(pipex->env);
 }
 
-void ft_init_pipex(t_args *pipex) {
-    // Initialize pipex structure
-    pipex->cmd1 = NULL;
-    pipex->cmd2 = NULL;
-    pipex->infile = NULL;
-    pipex->outfile = NULL;
+// Function to allocate memory and copy token
+static void	allocate_and_copy_token(char ***args, int count, const char *token)
+{
+	int	i;
+
+	*args = allocate_args(*args, count);
+	(*args)[count] = ft_strdup(token);
+	if (!(*args)[count])
+	{
+		i = 0;
+		while (i < count)
+		{
+			free((*args)[i]);
+			i++;
+		}
+		free(*args);
+		perror("Error: memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
 }
 
-char **split_command(const char *command) {
-    char **args = NULL;
-    char *cmd_copy = ft_strdup(command);
-    char *token = ft_strtok(cmd_copy, " ");
-    int count = 0;
+// Function to handle memory allocation and error checking
+char	**allocate_args(char **args, int count)
+{
+	int		i;
+	char	**new_args;
 
-    while (token) {
-        args = ft_realloc(args, sizeof(char *) * (count + 1));
-        args[count] = ft_strdup(token);
-        count++;
-        token = ft_strtok(NULL, " ");
-    }
-    args = ft_realloc(args, sizeof(char *) * (count + 1));
-    args[count] = NULL;
-
-    free(cmd_copy);
-    return args;
+	new_args = malloc(sizeof(char *) * (count + 1));
+	if (new_args == NULL)
+	{
+		i = 0;
+		while (i < count)
+		{
+			free(args[i]);
+			i++;
+		}
+		free(args);
+		perror("Error: memory allocation failed");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (i < count)
+	{
+		new_args[i] = args[i];
+		i++;
+	}
+	return (new_args);
 }
 
-void check_and_set_env() {
-    if (getenv("PATH") == NULL) {
-        setenv("PATH", "/usr/bin:/bin", 1);
-    }
-    // Add checks for other essential environment variables if needed
+// Function to tokenize the command string
+char	**tokenize_command(const char *command, int *count)
+{
+	char	**args;
+	char	*cmd_copy;
+	char	*token;
+
+	args = NULL;
+	cmd_copy = ft_strdup(command);
+	if (cmd_copy == NULL)
+	{
+		perror("Error: empty command");
+		exit(EXIT_FAILURE);
+	}
+	token = ft_strtok(cmd_copy, " ");
+	*count = 0;
+	while (token)
+	{
+		allocate_and_copy_token(&args, *count, token);
+		(*count)++;
+		token = ft_strtok(NULL, " ");
+	}
+	free(cmd_copy);
+	return (args);
 }
 
-void ft_parse(int argc, char **argv, t_args *pipex) {
-    if (argc != 5) {
-        ft_exit(pipex, "Usage: ./pipex infile command1 command2 outfile", EXIT_FAILURE);
-    }
-    pipex->infile = argv[1];
-    pipex->cmd1 = split_command(argv[2]);
-    pipex->cmd2 = split_command(argv[3]);
-    pipex->outfile = argv[4];
+static void	handle_child1(t_args *pipex, int pipe_fd[2])
+{
+	int	infile_fd;
 
-    // Check if infile is valid
-    int infile_fd = open(pipex->infile, O_RDONLY);
-    if (infile_fd == -1) {
-        ft_exit(pipex, "Error: Unable to open infile", EXIT_FAILURE);
-    }
-    close(infile_fd);
-
-    // Check and set essential environment variables
-    check_and_set_env();
+	infile_fd = open(pipex->infile, O_RDONLY);
+	if (infile_fd == -1)
+		ft_exit(pipex, "Error: Unable to open infile in child process",
+			EXIT_FAILURE);
+	dup2(infile_fd, STDIN_FILENO);
+	dup2(pipe_fd[1], STDOUT_FILENO);
+	close(infile_fd);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	execvp(pipex->cmd1[0], pipex->cmd1);
+	ft_exit(pipex, "Error: execvp failed for cmd1", EXEC_ERROR);
 }
 
-void ft_exec_cmd(t_args *pipex) {
-    int pipe_fd[2];
-    if (pipe(pipe_fd) == -1) {
-        ft_exit(pipex, "Error: Unable to create pipe", EXIT_FAILURE);
-    }
+// Function to handle the child process for the second command
+// Uses dup2 to redirect the input and output of the command
+// Closes the file descriptors and executes the command 
+static void	handle_child2(t_args *pipex, int pipe_fd[2])
+{
+	int	outfile_fd;
 
-    pid_t pid1 = fork();
-    if (pid1 == -1) {
-        ft_exit(pipex, "Error: Unable to fork process for cmd1", EXIT_FAILURE);
-    } else if (pid1 == 0) {
-        // Child process for command1
-        int infile_fd = open(pipex->infile, O_RDONLY);
-        if (infile_fd == -1) {
-            ft_exit(pipex, "Error: Unable to open infile in child process", EXIT_FAILURE);
-        }
-        dup2(infile_fd, STDIN_FILENO);
-        dup2(pipe_fd[1], STDOUT_FILENO);
-        close(infile_fd); // Close infile_fd after duplicating
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-        execvp(pipex->cmd1[0], pipex->cmd1);
-        ft_exit(pipex, "Error: execvp failed for cmd1", EXIT_FAILURE);
-    }
-
-    pid_t pid2 = fork();
-    if (pid2 == -1) {
-        ft_exit(pipex, "Error: Unable to fork process for cmd2", EXIT_FAILURE);
-    } else if (pid2 == 0) {
-        // Child process for command2
-        int outfile_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-        if (outfile_fd == -1) {
-            ft_exit(pipex, "Error: Unable to open outfile in child process", EXIT_FAILURE);
-        }
-        dup2(pipe_fd[0], STDIN_FILENO);
-        dup2(outfile_fd, STDOUT_FILENO);
-        close(outfile_fd); // Close outfile_fd after duplicating
-        close(pipe_fd[0]);
-        close(pipe_fd[1]);
-        execvp(pipex->cmd2[0], pipex->cmd2);
-        ft_exit(pipex, "Error: execvp failed for cmd2", EXIT_FAILURE);
-    }
-
-    // Parent process
-    close(pipe_fd[0]);
-    close(pipe_fd[1]);
-
-    int status1, status2;
-    if (waitpid(pid1, &status1, 0) == -1) {
-        ft_exit(pipex, "Error: waitpid failed for cmd1", EXIT_FAILURE);
-    }
-    if (waitpid(pid2, &status2, 0) == -1) {
-        ft_exit(pipex, "Error: waitpid failed for cmd2", EXIT_FAILURE);
-    }
-
-    if (status1 != 0) {
-        ft_exit(pipex, "Error: cmd1 failed", status1);
-    }
-
-    if (status2 != 0) {
-        ft_exit(pipex, "Error: cmd2 failed", status2);
-    }
+	outfile_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (outfile_fd == -1)
+		ft_exit(pipex, "Error: Unable to open outfile in child process",
+			EXIT_FAILURE);
+	dup2(pipe_fd[0], STDIN_FILENO);
+	dup2(outfile_fd, STDOUT_FILENO);
+	close(outfile_fd);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	execvp(pipex->cmd2[0], pipex->cmd2);
+	ft_exit(pipex, "Error: execvp failed for cmd2", EXEC_ERROR);
 }
 
-// Main function
-int main(int argc, char **argv) {
-    t_args pipex;
+// Function to handle the parent process
+// Closes the file descriptors and waits for the child processes to finish
+// If the child processes fail it displays an error message and exits
+static void	handle_parent(t_args *pipex, int pipe_fd[2], pid_t pid1, pid_t pid2)
+{
+	int	status1;
+	int	status2;
 
-    memset(&pipex, 0, sizeof(t_args));
-    printf("memset OK\n");
-    ft_init_pipex(&pipex);
-    printf("init OK\n");
-    ft_parse(argc, argv, &pipex);
-    printf("parse OK\n");
-    ft_exec_cmd(&pipex);
-    printf("exec OK\n");
-    ft_free_pipex(&pipex);
-    printf("free OK\n");
-    // Return the program
-    return (EXIT_SUCCESS);
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+	if (waitpid(pid1, &status1, 0) == -1)
+		ft_exit(pipex, "Error: waitpid failed for cmd1", EXEC_ERROR);
+	if (waitpid(pid2, &status2, 0) == -1)
+		ft_exit(pipex, "Error: waitpid failed for cmd2", EXEC_ERROR);
+	if (WIFEXITED(status1) == 0)
+		ft_exit(pipex, "Command 1 not found", ARG_ERROR);
+	if (WIFEXITED(status2) == 0)
+		ft_exit(pipex, "Command 2 not found", ARG_ERROR);
+}
+
+// Function to execute the commands
+// Creates a pipe and forks two child processes
+// The child proocesses are concurrent and execute the commands
+// The parent process waits for the child processes to finish
+void	ft_exec_cmd(t_args *pipex)
+{
+	int		pipe_fd[2];
+	pid_t	pid1;
+	pid_t	pid2;
+
+	if (pipe(pipe_fd) == -1)
+		ft_exit(pipex, "Error: Unable to create pipe", EXEC_ERROR);
+	pid1 = fork();
+	if (pid1 == -1)
+		ft_exit(pipex, "Error: Unable to fork process for cmd1", EXEC_ERROR);
+	else if (pid1 == 0)
+		handle_child1(pipex, pipe_fd);
+	pid2 = fork();
+	if (pid2 == -1)
+		ft_exit(pipex, "Error: Unable to fork process for cmd2", EXEC_ERROR);
+	else if (pid2 == 0)
+		handle_child2(pipex, pipe_fd);
+	handle_parent(pipex, pipe_fd, pid1, pid2);
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_args	pipex;
+
+	pipex.env = env;
+	ft_init_pipex(&pipex);
+	ft_parse(argc, argv, &pipex);
+	ft_exec_cmd(&pipex);
+	ft_free_pipex(&pipex);
+	return (EXIT_SUCCESS);
 }
